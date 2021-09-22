01a03b8ad97633077066455e292219719f7de25f
Basquiat <ememdaniel98@gmail.com>
2021-09-21 20:38:06 -0500

    final commit for the night, added and edited functions in java script, changed forms back to ol/li in html, added pseudocoding to js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,10 +4,24 @@ var answer1 = "";
 var answer2 = "";
 var answer3 = "";
 var answer4 = "";
-var correctAnswer = "";
+var numberCorrect = 0;
+var userSelection = "";
+
+//timer variables
+var timer = "";
+var timeLeft = "";
+
+//var timer
+// function nextQuestion(); {
+// if answer selected: run button event listener. run, bringOnTheNextQuestion.
+// }
+
+function emptyScreen() {
+    document.body.innerHTML = "";
+};
 
 function bringOnTheNextQuestion() {
-    var q = document.createElement("ol");
+    var q = document.createElement("li");
     var a1 = document.createElement("li");
     var a2 = document.createElement("li");
     var a3 = document.createElement("li");
@@ -23,31 +37,43 @@ function bringOnTheNextQuestion() {
     q.appendChild(a2);
     q.appendChild(a3);
     q.appendChild(a3);
+    console.log("it works");
+    //when button is clicked, display correct
+}
+
+function chooseAnAnswer() {
+    if (correctAnswer === answer1) {
+        numberCorrect + 1;
+
+        console.log("good job!");
+
+    } else if (correctAnswer != answer1) {
+        timeLeft - 10;
+        console.log("You'll get it next time!");
+        return
+    }
+    emptyScreen();
+}
 
 
-    function nextQuestion(); {
-        if (button === "clicked") {
-            question = "";
-            answer1 = "";
-            answer2 = "";
-            answer3 = "";
-            answer4 = "";
-            bringOnTheNextQuestion();
-        }
-    };
 
+function question1() {
+    question = "what is question one";
+    answer1 = "answer1";
+    answer2 = "answer2";
+    answer3 = "answer3";
+    answer4 = "answer4";
+    correctAnswer = answer1;
 
-    function Question1() {
-        question = "what is question one";
-        answer1 = "answer 1";
-        answer2 = "answer 2";
-        answer3 = "answer3";
-        answer4 = "answer4";
-        correctAnswer = answer1;
+    if (correctAnswer === answer1) {
+        numberCorrect + 1;
 
-        if (correctAnswer === answer1) {
-            console.log("good job!");
-        } else if (correctAnswer != answer1) {
-            console.log("You'll get it next time!");
-        }
-    };
+        console.log("good job!");
+
+    } else if (correctAnswer != answer1) {
+        timeLeft - 10;
+        console.log("You'll get it next time!");
+    }
+};
+
+bringOnTheNextQuestion();
